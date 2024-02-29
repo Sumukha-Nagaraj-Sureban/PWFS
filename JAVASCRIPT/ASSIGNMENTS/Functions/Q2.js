90e38f4e6f5257772522b9bdb5db6a1b907caeb2
dev-sumukha <sumukhasureban2002@gmail.com>
2024-02-29 15:02:04 +0530

    add Q2.js

---
+++ JAVASCRIPT/ASSIGNMENTS/Functions/Q2.js
@@ -0,0 +1,12 @@
+function generateGreeting(name) {
+    return `Hello, ${name}! Welcome to our community.`;
+}
+
+// Example usage:
+const person1 = "Sumukha";
+const person2 = "Varun";
+const person3 = "Parth";
+
+console.log(generateGreeting(person1));
+console.log(generateGreeting(person2));
+console.log(generateGreeting(person3));
